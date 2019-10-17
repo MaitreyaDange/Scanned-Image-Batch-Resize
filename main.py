c4954cc4490abc6319fccaae320764e3a6f95323
Maitreya Dange <madmaitreya@gmail.com>
2019-10-17 09:32:03 +0530

    version 1.0

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 from PIL import Image
+from PIL import ImageEnhance
+from tqdm import tqdm
 
 # Paste the path of the directory containing files
 IN_PATH = 'C:\\Users\\Maitreya Dange\\Documents\\Coding\\Python Projects\\Scanned Image Batch Resize\\input\\'
 OUT_PATH = 'C:\\Users\\Maitreya Dange\\Documents\\Coding\\Python Projects\\Scanned Image Batch Resize\\output\\'
+# DecompressionBombing Error Suppression :
+Image.MAX_IMAGE_PIXELS = 180000000
+
+# Set Parameters
+# Resize Factor < 1
+RESIZE_FACTOR = 1/6
+
+# Contrast
+CONTRAST = 1.5
 
 files = os.listdir(IN_PATH)
 
@@ -15,11 +26,17 @@ def rename_file() :
         os.rename(os.path.join(OUT_PATH, file), os.path.join(OUT_PATH, file[:len(file)-4] +  ' renamed.png'))
 
 def resize_image():
-    for file in files :
-        if os.path.isfile(IN_PATH+file) :
-            im = Image.open(IN_PATH+file)
-            im = im.resize((int(im.size[0]/10),int(im.size[1]/10)), Image.LANCZOS)
-            im.save( OUT_PATH+file[:len(file)-4] + ' resized.jpg', 'JPEG')
+    with tqdm(total = len(files)) as pbar :
+        for file in files :
+            if os.path.isfile(IN_PATH+file) :
+                im = Image.open(IN_PATH+file)
+                enh_im = ImageEnhance.Contrast(im)
+                im = enh_im.enhance(CONTRAST)
+                im = im.resize((int(im.size[0]*RESIZE_FACTOR),int(im.size[1]*RESIZE_FACTOR)), Image.LANCZOS)
+                im = im.convert("RGB")
+                im.save( OUT_PATH+file[:len(file)-4] + ' resized.jpg', 'JPEG')
+            pbar.update(1)
+        pbar.close()   
   
 # Driver Code 
 if __name__ == '__main__': 
